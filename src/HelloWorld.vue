c6ae03131f9911ecc5e39e3cd62df68be1bfec76
Civan <civan.erbay@web.de>
2020-10-18 11:38:53 +0200

    Add LifeCycle Methods

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -12,10 +12,36 @@ export default {
       district: "Ehrenfeld",
     };
   },
-  mounted() {
-    /* debugger */
-    this.myName = "Peter Pan"
+  beforeCreate() {
     console.log(this.myName);
+    console.log("This is beforeCreate");
+  },
+  created() {
+      //Data Injections are Done but (Data is Avalaible) but our View(Template) is NOT available
+    console.log("This is created");
+    debugger;
+    console.log(this.myName);
+  },
+  beforeMount() {
+    console.log("This is beforeMount");
+    debugger
+    console.log(this.myName);
+  },
+  mounted() {
+      //View(Template) gets iniated and displayed! 
+      debugger
+    this.myName = "Peter Pan";
+    console.log(this.myName);
+  },
+
+
+  beforeUnmount() {
+    //Works only when navigating away from this page e.g. with Router
+    console.log("This is beforeMount");
+  },
+  unmounted() {
+    //Works only when navigating away from this page e.g. with Router
+    console.log("This is unmounted");
   },
 };
 </script>
