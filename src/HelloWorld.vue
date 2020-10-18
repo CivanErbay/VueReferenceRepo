4fefb7924131438cb4b52bae2340e110ef8cb515
Civan <civan.erbay@web.de>
2020-10-18 23:03:16 +0200

    Add Final Project

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -1,39 +1,89 @@
 <template>
-  <h1>{{ inputValue }}</h1>
+  <h1>Speed Typer</h1>
 
-  <h1>{{ keywords.join(" | ") }}</h1>
-  <!--   function($event) Syntax - very helpful to define custom functions with user Input, other than simplified v-model for data binding
+<!--     Very Interesting Filter Method to get the Number of the Correct Values -> by filtering out an Array of only Correct Answers
  -->
+  <p> Your Score: {{keywords.filter(keyword => 
+    keyword.correct).length }} / {{keywords.length}} </p>
+  <p> 
+<!--     :class Condition is defined by the truthy Value of the object depending on the keyword. By default for example all every keyword has a class="pending"
+ -->    <span :class="{ correct: keyword.correct, wrong: keyword.wrong, pending: keyword.pending}" v-for="keyword in keywords" :key="keyword">
+      {{ keyword.text }} {{ " " }}
+    </span>
 
-  <!--   THIS is CHANGING THE VALUES WHILE TYPING
- -->
-  <input :value="inputValue" @input="processInput($event)" />
-
-  <!--  USE of Space to submit Value -->
-  <input :value="inputValue" @keyup.space="processInput($event)" />
-      <!-- Use of alt + enter to submit Value -->
-    <input :value="inputValue" @keyup.alt.enter="processInput($event)" />
+    <input
+      type="text"
+      :value="inputValue"
+      @keyup.space="processInput($event)"
+    />
+  </p>
 </template>
 
 
 <script>
+/* VERY NICE APPROACH TO MAKE STRINGS TO OBJECTS WITH SEVERAL PROPERTYS */
+const defaultKeywords = [
+  "facebook",
+  "google",
+  "denkwerk",
+  "sony",
+  "vue",
+  "react",
+  "angular",
+  "webpack",
+  "netflix",
+  "frisbee",
+].map((keyword) => {
+  return {
+    text: keyword,
+    correct: false,
+    wrong: false,
+    pending: true,
+  };
+});
+
 export default {
   data() {
     return {
       inputValue: "",
       index: 0,
-      keywords: [],
+      keywords: defaultKeywords,
     };
   },
   methods: {
     processInput(event) {
-      const value = event.target.value;
-      this.keywords[this.index] = value; //same like push, because mutating the array at specific positions
-      (this.inputValue = ""), this.index++;
+      const value = event.target.value.trim();
+      if (value == "") {
+        return;
+      }
+
+      if (this.keywords[this.index].text === value) {
+        this.keywords[this.index].correct = true;
+        this.keywords[this.index].wrong = false;
+        this.keywords[this.index].pending = false;
+      } else {
+        this.keywords[this.index].correct = false;
+        this.keywords[this.index].wrong = true;
+        this.keywords[this.index].pending = false;
+      }
+
+      this.inputValue = "";
+      this.index++;
     },
   },
 };
 </script>
 
 <style scoped>
+.pending {
+  font-weight: bold;
+}
+.correct {
+  font-weight: bold;
+  color: green;
+}
+.wrong {
+  font-weight: bold;
+  color: red;
+}
 </style>
