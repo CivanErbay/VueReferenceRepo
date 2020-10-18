f1a1d6f911e30f95e79404d64033ea61d9f3cd80
Civan <civan.erbay@web.de>
2020-10-18 11:21:02 +0200

    Add and Access Data

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -1,20 +1,24 @@
 <template>
-    <h1>Hello World</h1>
-    <p>My Name is {{myName}} and I live in {{district}}</p>
+  <h1>Hello World</h1>
+  <p>My Name is {{ myName }} and I live in {{ district }}</p>
 </template>
 
 
 <script>
 export default {
-    data() {
-        return {
-            myName: "Civan Erbay",
-            district: "Ehrenfeld"
-        }
-    }
-}
+  data() {
+    return {
+      myName: "Civan Erbay",
+      district: "Ehrenfeld",
+    };
+  },
+  mounted() {
+    /* debugger */
+    this.myName = "Peter Pan"
+    console.log(this.myName);
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
