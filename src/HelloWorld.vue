f32da24cbe96f6903227a8e5cdf2dfefd334f5d9
Civan <civan.erbay@web.de>
2020-10-18 11:11:22 +0200

    Init Vue Reference Projecg

---
+++ src/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Hello World</h1>
+    <p>My Name is {{myName}} and I live in {{district}}</p>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            myName: "Civan Erbay",
+            district: "Ehrenfeld"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
