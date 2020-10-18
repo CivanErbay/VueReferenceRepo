fb85027d40d2491c9357e53b57796c14fe6f9c4d
Civan <civan.erbay@web.de>
2020-10-18 22:24:22 +0200

    Add EventModifiers

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -1,37 +1,18 @@
 <template>
-  <div class="one" align="center">
-    <h2>One Way Data Binding</h2>
-    <!--       v-on is kind of an Event-Listener
-        We can also use Expressions like  {{ XY }} to display data
-        But to bind data to specific fields like Inputs you need to use v-bind -->
-    <input value="randomNumber" />
-    <!--    Additional Info : The Following is "only" One-Way-Data-binding -->
-    <input v-bind:value="randomNumber" />
-    <input :value="randomNumber" />
-    <!-- same -->
-    <button @click="oneWayBinding">BindMeOneWayOnly</button>
-    <p>
-      As you can see - I can bind the value via Button-Method, but I can not
-      bind it from the input the other way round. v-bind can "only" display
-      values
-    </p>
-  </div>
-  <div class="one" align="center">
-    <h2>Two Way Data Binding</h2>
-    <div class="flexy">
-      <h3>v-on:Change --> Adds Value onEnter</h3>
-      <input v-on:change="twoWayBinding($event)" />
-    </div>
-    <div class="flexy">
-      <h3>@Input Syntax --> Adds Value onChange directly</h3>
-      <input @input="twoWayBinding($event)" />
-    </div>
-      <div class="flexy">
-      <h3>v-model --> Easiest approach for 2-Way Data Binding IMO</h3>
-      <input v-model="randomNumber" />
-    </div>
-    <h2>Result {{ randomNumber }}</h2>
-  </div>
+  <h1>{{ inputValue }}</h1>
+
+  <h1>{{ keywords.join(" | ") }}</h1>
+  <!--   function($event) Syntax - very helpful to define custom functions with user Input, other than simplified v-model for data binding
+ -->
+
+  <!--   THIS is CHANGING THE VALUES WHILE TYPING
+ -->
+  <input :value="inputValue" @input="processInput($event)" />
+
+  <!--  USE of Space to submit Value -->
+  <input :value="inputValue" @keyup.space="processInput($event)" />
+      <!-- Use of alt + enter to submit Value -->
+    <input :value="inputValue" @keyup.alt.enter="processInput($event)" />
 </template>
 
 
@@ -39,28 +20,20 @@
 export default {
   data() {
     return {
-      randomNumber: Math.random(),
+      inputValue: "",
+      index: 0,
+      keywords: [],
     };
   },
-  //Methods is just a simple plain Object, because we define the methods inside of this objects - Those will be functions
   methods: {
-    oneWayBinding() {
-      this.randomNumber = 100;
-    },
-    twoWayBinding(e) {
-      this.randomNumber = e.target.value;
+    processInput(event) {
+      const value = event.target.value;
+      this.keywords[this.index] = value; //same like push, because mutating the array at specific positions
+      (this.inputValue = ""), this.index++;
     },
   },
 };
 </script>
 
 <style scoped>
-.one {
-  padding: 2em;
-}
-.flexy {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 </style>
