6e06548770c88c64c951fcdc14b811fccf16c3ac
Civan <civan.erbay@web.de>
2020-10-20 10:27:24 +0200

    Clean Main branch

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -11,38 +11,7 @@ export default {
       myName: "Civan Erbay",
       district: "Ehrenfeld",
     };
-  },
-  beforeCreate() {
-    console.log(this.myName);
-    console.log("This is beforeCreate");
-  },
-  created() {
-      //Data Injections are Done but (Data is Avalaible) but our View(Template) is NOT available
-    console.log("This is created");
-    debugger;
-    console.log(this.myName);
-  },
-  beforeMount() {
-    console.log("This is beforeMount");
-    debugger
-    console.log(this.myName);
-  },
-  mounted() {
-      //View(Template) gets iniated and displayed! 
-      debugger
-    this.myName = "Peter Pan";
-    console.log(this.myName);
-  },
-
-
-  beforeUnmount() {
-    //Works only when navigating away from this page e.g. with Router
-    console.log("This is beforeMount");
-  },
-  unmounted() {
-    //Works only when navigating away from this page e.g. with Router
-    console.log("This is unmounted");
-  },
+  }
 };
 </script>
 
